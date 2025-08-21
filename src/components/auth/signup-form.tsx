b29579092a001827e35475995c4643cfba1c5983
Vaibhav Murmu <100757195+Vaibhavmurmu@users.noreply.github.com>
2025-08-22 00:28:46 +0530

    Update signup-form.tsx

--- src/components/auth/signup-form.tsx
+++ src/components/auth/signup-form.tsx
@@ -11,7 +11,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-export function SignUp() {
+export function CardDemo() {
   return (
     <Card className="w-full max-w-sm">
       <CardHeader>
