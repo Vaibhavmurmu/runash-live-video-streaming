c88701fcbef958601f3c578b24b721de3b6ac621
Vaibhav Murmu <100757195+Vaibhavmurmu@users.noreply.github.com>
2025-08-21 13:48:17 +0530

    Update logo.tsx

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ export function Logo() {
       <span className="text-cyan-600">a</span>
       <span className="text-teal-400">i</span>
       <span className="mx-1">&middot;</span>
-      <span>vstudio</span>
+      <span>studio</span>
     </>
   );
 }
