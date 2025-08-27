f4fa03d46d24a9d9fc7ff865064192600de2e935
Vaibhav Murmu <100757195+Vaibhavmurmu@users.noreply.github.com>
2025-08-28 01:26:55 +0530

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,7 +8,7 @@ export default function LoginPage() {
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-            <GalleryVerticalEnd className="size-4" />
+            {/* <GalleryVerticalEnd className="size-4" /> */}
           </div>
           RunAsh AI.
         </a>
